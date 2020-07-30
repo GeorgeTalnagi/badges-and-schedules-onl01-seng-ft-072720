b275c91844a9b83db153615d12c8485908f2a216
George Talnagi <gtalnagi@yahoo.com>
2020-07-30 16:40:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 def assign_rooms(speakers)
   new_speakers = []
-  speakers.each do ||
+  speakers.each_with_index do ||
     new_speakers.push ("Hello, #{}! You'll be assigned to room #{}!")
   end
   return new_speakers
